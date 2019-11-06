ae36d6a50bf490e29a24e5b960dfd40a8e86569a
josepaniagua <jpanjr@gmail.com>
2019-11-06 00:01:14 -0800

    select in create file

--- team-builder/src/Components/Create.js
+++ team-builder/src/Components/Create.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card } from './TeamList.js';
+// import { Card } from './TeamList.js';
 import Button from "react-bootstrap";
 
 const Create = props => {
@@ -22,7 +22,27 @@ const Create = props => {
         setNewMember(initialMember);
     };
 
-
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <CenteredForm>
+                    <h2>New Team Member</h2>
+                    <div>
+                        <label for='name'>Name</label>
+                        <input type='text' name='name' className='name' onChange={handleChange} placeholder='John Smith' value={newMember.name} />
+                    </div>
+                    <div className='input-stack'>
+                        <label htmlFor='nickname'>Nickname</label>
+                        <input type='text' name='nickname' className='nickname' onChange={handleChange} placeholder='Jonny' value={newMember.nickname}/>
+                    </div>
+                    <div className='input-stack'>
+                        <label for='role'>Role</label>
+                        <select></select>
+                    </div>
+                </CenteredForm>
+            </form>
+        </div>
+    );
 
 
 };
