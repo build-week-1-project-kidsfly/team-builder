cfa258fcd674dfd147c62bc5fd00d8cb4cb30f7f
josepaniagua <jpanjr@gmail.com>
2019-11-05 18:05:59 -0800

    teammember template

---
+++ team-builder/src/Components/TeamMember.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TeamMember = props => {
+    const { name, nickname, role, email } = props;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h4>({nickname})</h4>
+            <h4>Role: {role}</h4>
+            <h4>Email: {email}</h4>
+        </div>
+    );
+};
+
+export default TeamMember;
