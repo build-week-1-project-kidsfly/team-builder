d114fcb189d52d13a4f5ff7941d2ec934c6e7efa
josepaniagua <jpanjr@gmail.com>
2019-11-05 23:12:54 -0800

    teammember

---
+++ team-builder/src/Components/TeamList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TeamMember from "./TeamMember";
+
+
+const TeamList = props => {
+    const { data } = props;
+    return(
+        <div>
+            {data.map((member, index) => {
+                const { name, nickname, role, email } = member;
+                return (
+                    <TeamMember key={index} name={name} nickname={nickname} role={role} email={email} />
+                );
+            })}
+        </div>
+    );
+};
+
+export default TeamList;
--- team-builder/src/Components/TeamMember.js
+++ team-builder/src/Components/TeamMember.js
@@ -7,7 +7,7 @@ const TeamMember = props => {
             <h1>{name}</h1>
             <h4>({nickname})</h4>
             <h4>Role: {role}</h4>
-            <h4>Email: {email}</h4>
+            <h4>Email: {email           }</h4>
         </div>
     );
 };
