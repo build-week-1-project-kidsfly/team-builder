89789cb971267b97dc14424c5cce147660a38590
josepaniagua <jpanjr@gmail.com>
2019-11-06 00:09:01 -0800

    create file

--- team-builder/src/Components/Create.js
+++ team-builder/src/Components/Create.js
@@ -37,9 +37,25 @@ const Create = props => {
                     </div>
                     <div className='input-stack'>
                         <label for='role'>Role</label>
-                        <select></select>
+                        <select className='role' onChange={handleChange}>
+                            <option>Choose One</option>
+                            <option>Server Administrator</option>
+                            <option>Quality Assurance Tester</option>
+                            <option>Web Content Specialist</option>
+                            <option>Frontend Developer</option>
+                            <option>Backend Developer</option>
+                            <option>UI Designer</option>
+                            <option>UX Designer</option>
+                            <option>Information Architect</option>
+                        </select>
+                    </div>
+                    <div className='input-stack'>
+                        <label htmlFor='email'>Email Address</label>
+                        <input type='text' name='email' className='email' onChange={handleChange} placeholder='name123@email.com' value={newMember.email}/>
                     </div>
                 </CenteredForm>
+                <Button className='formButton' type='submit' variant='dark'>Submit</Button>
+                <Button type='button' onClick={resetForm} variant='dark'>Reset</Button>
             </form>
         </div>
     );
