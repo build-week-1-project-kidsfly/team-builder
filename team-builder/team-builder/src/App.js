2f79d98c1a659860864c43a210f7da4bae4ae2ed
josepaniagua <jpanjr@gmail.com>
2020-01-24 00:53:01 -0800

    deleted new team h1

--- team-builder/team-builder/src/App.js
+++ team-builder/team-builder/src/App.js
@@ -8,9 +8,8 @@ const App = () => {
     console.log(team);
     return (
         <div className='hello'>
-            <h1 className='align'>New Members</h1>
             <Form setNewMember={setTeam} members={team}  />
-            <h1 className='align'>Original Members</h1>
+            <h1 className='align'>Development Team</h1>
             <MemberList groupData={team} />
         </div>
     );
